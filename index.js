621ea837d201224f4530252e216334afce5a37d9
Emilio Resende <emilio.murta@msitbrasil.com>
2023-09-10 11:25:19 -0300

    + add table endpoint

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-import { Person } from './person.js';
+import { Person } from "./person.js";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json(), cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/table", (req, res) => {
+  const table = {
+    0: "Magreza",
+    18.5: "Normal",
+    24.9: "Sobrepeso",
+    99: "Obesidade",
+  };
+  res.send(JSON.stringify(table));
+});
 
 app.post("/imc/calculate", (req, res) => {
-    const {
-        height, 
-        weight
-    } = req.body;
-    
-    const person = new Person(height, weight)
-        .withImc()
-        .withImcDescription();
-    
-    res.send(JSON.stringify(person));
+  const { height, weight } = req.body;
+
+  const person = new Person(height, weight).withImc().withImcDescription();
+
+  res.send(JSON.stringify(person));
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
